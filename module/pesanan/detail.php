0be0c56f39ff269f387abb12469b5216c6ab37af
Marvvllo <marvtfs@gmail.com>
2022-03-16 08:02:24 +0800

    [ADD] Tambahkan page detail pesanan

--- module/pesanan/detail.php
+++ module/pesanan/detail.php
@@ -2,112 +2,41 @@
 
 	$pesanan_id = $_GET["pesanan_id"];
 
-	$query = mysqli_query($koneksi, "SELECT pesanan.nama_penerima, pesanan.nomor_telepon, pesanan.alamat, pesanan.tanggal_pemesanan, user.nama, kota.kota, kota.tarif
-																	FROM pesanan JOIN user ON pesanan.user_id=user.user_id JOIN kota ON kota.kota_id=pesanan.kota_id WHERE pesanan.pesanan_id='$pesanan_id'");
-
+	$query = mysqli_query($koneksi, "SELECT pesanan.*, user.nama, pesanan_detail.*, game.nama_game, game.spesifikasi
+																		FROM pesanan 
+																		JOIN user on pesanan.user_id = user.user_id 
+																		JOIN pesanan_detail ON pesanan.pesanan_id = pesanan_detail.pesanan_id
+																		JOIN game on pesanan_detail.game_id = game.game_id 
+																		WHERE pesanan.pesanan_id='1'");
 	$row = mysqli_fetch_assoc($query);
 
-	$tanggal_pemesanan = $row['tanggal_pemesanan'];
-	$nama_penerima = $row['nama_penerima'];
-	$nomor_telepon = $row['nomor_telepon'];
-	$alamat = $row['alamat'];
-	$tarif = $row['tarif'];
+	$nama_game = $row['nama_game'];
+	$tanggal_pemesanan = date("m-d-Y", strtotime($row['tanggal_pemesanan']));
+	$spesifikasi = $row['spesifikasi'];
 	$nama = $row['nama'];
-	$kota = $row['kota'];
-
+	$quantity = $row['quantity'];
+	$qrImage = urlencode(BASE_URL . "index.php?page=my_profile&module=pesanan&action=detail&pesanan_id=$pesanan_id");
 ?>
 
-<div id="frame-faktur">
-
-	<h3 align='center'>Detail Pesanan</h3>
-
-	<hr/>
-
-	<table>
-
-		<tr>
-			<td>Nomor Faktur</td>
-			<td>:</td>
-			<td><?php echo $pesanan_id ?></td>
-		</tr>
-		<tr>
-			<td>Nama Pemesan</td>
-			<td>:</td>
-			<td><?php echo $nama?></td>
-		</tr>
-		<tr>
-			<td>Nama Penerima</td>
-			<td>:</td>
-			<td><?php echo $nama_penerima?></td>
-		</tr>
-		<tr>
-			<td>Alamat</td>
-			<td>:</td>
-			<td><?php echo $alamat?></td>
-		</tr>
-		<tr>
-			<td>Nomor Telepon</td>
-			<td>:</td>
-			<td><?php echo $nomor_telepon?></td>
-		</tr>
-		<tr>
-			<td>Tanggal Pemesanan</td>
-			<td>:</td>
-			<td><?php echo $tanggal_pemesanan?></td>
-		</tr>
-	</table>
-</div>
-
-<table class="table-list">
-
-	<tr class="baris-title">
-		<th class="no">No</th>
-		<th class="kiri">Nama Barang</th>
-		<th class="tengah">Qty</th>
-		<th class="kanan">Harga Satuan</th>
-		<th class="kanan">Total</th>
-	</tr>
-
-	<?php
+<div class="detail-pesanan">
+		<div class="keterangan-pesanan">
+			<h2><?php echo $nama_game ?></h2>
+			<p><?php echo $tanggal_pemesanan ?></p>
+			<p><?php echo $spesifikasi ?></p>
+			<p><?php echo $nama ?></p>
+			<p><?php echo $quantity ?> Kursi</p>
+		</div>
+		<?php
+			if ($row['status'] == 3) {
+				echo "<img src='https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=$qrImage' />";
+			} else {
+				echo "<div id='frame-keterangan-pembayaran'>
+								<p>
+									Setelah melakukan pembayaran silahkan lakukan konfirmasi pembayaran
+									<a href='echo" . BASE_URL . "index.php?page=my_profile&module=pesanan&action=konfirmasi_pembayaran&pesanan_id='$pesanan_id'>Disini</a>
+								</p>
+							</div>";
+			}
+		?>
+	</div>
 	
-		$queryDetail = mysqli_query($koneksi, "SELECT pesanan_detail.*, barang.nama_barang FROM pesanan_detail JOIN barang ON
-																					pesanan_detail.barang_id=barang.barang_id WHERE pesanan_detail.pesanan_id='$pesanan_id'");
-	
-		$no = 1;
-		$subtotal = 0;
-
-		while($rowDetail = mysqli_fetch_assoc($queryDetail)) {
-
-			$total = $rowDetail['harga'] * $rowDetail['quantity'];
-			$subtotal += $total;
-
-			echo "<tr>
-							<th class='no'>$no</th>
-							<th class='kiri'>$rowDetail[nama_barang]</th>
-							<th class='tengah'>$rowDetail[quantity]</th>
-							<th class='kanan'>".rupiah($rowDetail['harga'])."</th>
-							<th class='kanan'>".rupiah($total)."</th>
-						</tr>";
-			$no++;
-		}
-		$subtotal += $tarif;
-	?>
-	<tr>
-		<td class="kanan" colspan="4">Biaya Pengiriman</td>
-		<td class="kanan"><?php echo rupiah($tarif); ?></td>
-	</tr>
-
-	<tr>
-		<td class="kanan" colspan="4">Sub total</td>
-		<td class="kanan"><b><?php echo rupiah($subtotal); ?></b></td>
-	</tr>
-	
-</table>
-
-<div id="frame-keterangan-pembayaran">
-	<p>Silahkan Lakukan pembayaran ke Bank MNR<br />
-		Nomor Account : 1400-0999-1600 (A/N KeebShopByKel3).<br />
-		Setelah melakukan pembayaran silahkan lakukan konfirmasi pembayaran
-		<a href="<?php echo BASE_URL . "index.php?page=my_profile&module=pesanan&action=konfirmasi_pembayaran&pesanan_id='$pesanan_id'"; ?>">Disini</a>
-	</p>
-</div>
