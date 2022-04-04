e9e21c5b53cce65628951ae50cd364a1c576c7df
Marvvllo <marvtfs@gmail.com>
2022-04-04 08:15:29 +0800

    [FIX] proses_pesanan.php

--- proses_pesanan.php
+++ proses_pesanan.php
@@ -5,8 +5,10 @@
     session_start();
 
 		$user_id = $_SESSION['user_id'];
-    $tanggal = date('Y-m-d');
-
+    $game_id = $_POST['game_id'];
+    $queryGame = mysqli_query($koneksi, "SELECT * FROM game WHERE game_id='$game_id' AND status='on'");
+    $rowGame = mysqli_fetch_array($queryGame);
+    $tanggal = $rowGame['tanggal_game'];
 
     if (!$user_id) {
 			header("Location:".BASE_URL."index.php?page=login");
@@ -16,17 +18,19 @@
                                     VALUES (NULL, $user_id, '$tanggal', '0'); ");
     echo $tanggal;
     if($query){
-        $game_id = $_POST['game_id'];
+        $last_pesanan_id = mysqli_insert_id($koneksi);
+        
+        
         $quantity = $_POST["quantity"];
         $harga = $_POST["harga"];
 				$queryDetail =  mysqli_query($koneksi, "INSERT INTO pesanan_detail(pesanan_id, game_id, quantity, harga)
-				VALUES (LAST_INSERT_ID(), '$game_id', '$quantity', '$harga' )");
+				VALUES ($last_pesanan_id, '$game_id', '$quantity', '$harga' )");
     } else{
       echo "
           <p>Pesanan anda gagal, mohon coba lagi.</p>
           ";
     }
     
-    header("Location:".BASE_URL."index.php?page=my_profile&module=pesanan&action=detail&pesanan_id=$pesanan_id");
+    header("Location:".BASE_URL."index.php?page=my_profile&module=pesanan&action=detail&pesanan_id=$last_pesanan_id");
     
 ?>
